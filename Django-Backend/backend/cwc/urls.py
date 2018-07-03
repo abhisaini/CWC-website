57626f267a6eca7f8b3211eda5ac760608e985d7
abhsaini <abhsni@iitk.ac.in>
2018-07-03 21:42:45 +0530

    added django project

---
+++ Django-Backend/backend/cwc/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+            # Your mapping must be in follwing format
+            # path('', views.viewname, name='somename'),
+            ]
