3616011c1a9d5c9c2478f9424dc7d7d8d076c1c6
abhsaini <abhsni@iitk.ac.in>
2018-07-20 07:57:35 +0530

    added endpoints for create/delete/get posts

--- Django-Backend/backend/cwc/models.py
+++ Django-Backend/backend/cwc/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class User(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
     password = models.CharField(max_length=100)
-
+    
     # def login
 # Create your models here.
 class Post(models.Model):
